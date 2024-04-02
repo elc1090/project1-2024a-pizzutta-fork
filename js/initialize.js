dac2ebedde73b159b0e0cf909bbce2aeede6dc46
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-04-01 22:15:37 -0300

    feat: copy link

--- js/initialize.js
+++ js/initialize.js
@@ -35,9 +35,22 @@ $(document).ready(function () {
 
   $('input[type=radio][name=signatureType]').change(signature.handleSignatureTypeChange)
 
+  initializeFields()
   signature.initialize()
   validation.initialize()
 });
+
+const initializeFields = () => {
+  const params = new URLSearchParams(window.location.search)
+  params.forEach((value, key) => {
+    if (key === "period") {
+      $(`input[name=${key}][value=${value}]`).prop('checked', true)
+    } else {
+      $(`input[name=${key}]`).val(value)
+    }
+  })
+  M.updateTextFields()
+}
 
 const calculateFinalValue = () => {
   const relevanceAndOriginality = $(`#relevanceAndOriginalityValue`).text()
@@ -60,4 +73,18 @@ const resetAllFields = () => {
   $('#signatureCanvasWrapper').hide()
   $('#signaturePreview').hide()
   $('#signatureSelect').hide()
+}
+
+const copyLink = () => {
+  const data = generatePDF.getData()
+
+  const link = `index.html?studentName=${encodeURIComponent(data.generalInformation.studentName)}`
+    + `&professorName=${encodeURIComponent(data.generalInformation.professorName)}`
+    + `&period=${data.generalInformation.period}`
+    + `&date=${encodeURIComponent(data.generalInformation.date)}`
+    + `&time=${encodeURIComponent(data.generalInformation.time)}`
+
+  navigator.clipboard.writeText(link)
+
+  M.toast({html: `<i class="material-icons">check</i><span>&nbsp;Copiado para área de transferência</span>`})
 }
